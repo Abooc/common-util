0c4200840be502d5712149330088f22afa7108d6
ruiyulee@hotmail.com <788959git>
2016-11-25 17:21:29 +0800

    delete app.iml

--- app/src/main/java/com/abooc/debug/samples/AppApplication.java
+++ app/src/main/java/com/abooc/debug/samples/AppApplication.java
@@ -13,13 +13,18 @@ public class AppApplication extends Application {
 
     @Override
     public void onCreate() {
+        // 设置Debug是否可用
         Debug.enable(BuildConfig.DEBUG);
         super.onCreate();
 
+        // 锚点
         Debug.anchor();
 
+        // debug本类，将会以本类名作为日志TAG(标签)
         Debug.debugClass();
+        // 此时锚点打印，将会以本类名作为日志TAG(标签)
         Debug.anchor();
-
+        // 停止将本类名作为日志TAG
+        Debug.destroyClass();
     }
 }
